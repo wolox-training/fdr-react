662acb4f540b2810a761e8f27c9bb4b44e4cb2a3
Federico Di Rocco <fede.dirocco@hotmail.com>
2018-11-23 13:00:13 -0300

    Implemented taking turns. Changed class Square to function.

--- training-react/src/app/components/Board/index.js
+++ training-react/src/app/components/Board/index.js
@@ -8,24 +8,28 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
-  handleClick(i) {
+  handleClick = i => {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares });
-  }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      squares,
+      xIsNext: !this.state.xIsNext 
+    });
+  };
 
   renderSquare = i =>
-    <Square 
+    <Square
       value={this.state.squares[i]}
       onClick={() => this.handleClick(i)}
     />;
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
