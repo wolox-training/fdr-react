49134588054faabe27ebb85905aa1428886eab9b
Federico Di Rocco <fede.dirocco@hotmail.com>
2018-11-23 15:47:27 -0300

    Implemented showing past moves. Game component had the implementation and status-history moves.

--- training-react/src/app/components/Board/index.js
+++ training-react/src/app/components/Board/index.js
@@ -4,60 +4,15 @@ import Square from '../Square';
 
 import styles from './styles.scss';
 
-class Board extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    };
-  }
-
-  handleClick = i => {
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({ 
-      squares,
-      xIsNext: !this.state.xIsNext 
-    });
-  };
-
-  calculateWinner = squares => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
+class Board extends Component {  
 
   renderSquare = i =>
     <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
     />;
 
   render() {
-    const winner = this.calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
         <div className={styles.status}>{status}</div>
