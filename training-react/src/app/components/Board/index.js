da7ea77f5531a7ce33b2b90b178eafb825cf46d9
Federico Di Rocco <fede.dirocco@hotmail.com>
2018-11-23 14:22:43 -0300

    Implemented declaring a winner. Added calculateWinner function in Board.

--- training-react/src/app/components/Board/index.js
+++ training-react/src/app/components/Board/index.js
@@ -22,6 +22,27 @@ class Board extends Component {
     });
   };
 
+  calculateWinner = squares => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
   renderSquare = i =>
     <Square
       value={this.state.squares[i]}
@@ -29,7 +50,13 @@ class Board extends Component {
     />;
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
