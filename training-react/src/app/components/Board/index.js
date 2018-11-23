a681c89341d8e41f12fc4903a48065e6946ca8bd
Federico Di Rocco <fede.dirocco@hotmail.com>
2018-11-23 17:11:09 -0300

    Added proptypes to Board and Square components

--- training-react/src/app/components/Board/index.js
+++ training-react/src/app/components/Board/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Square from '../Square';
 
@@ -34,6 +35,11 @@ class Board extends Component {
       </div>
     );
   }
+}
+
+Board.propTypes = {
+  squares: PropTypes.arrayOf(PropTypes.number),
+  onClick: PropTypes.func
 }
 
 export default Board;
