04266c36ebe9c75cccdcff0dd3b683011141e3d0
Federico Di Rocco <fede.dirocco@hotmail.com>
2018-12-20 10:54:48 -0300

    Implemeted private routes for home and game

--- training-react/src/app/components/App/index.js
+++ training-react/src/app/components/App/index.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Game from '../Game';
-import LocalStoreService from '../../../services/LocalStoreService';
+import { PrivateRoute } from '../PrivateRoute';
 import './styles.scss';
-import LoginForm from '../LoginForm';
-
-const USER_SESSION = 'USER_SESSION';
 
 class App extends Component {
   render() {
-    const user = JSON.parse(LocalStoreService.getItem(USER_SESSION));
     return (
       <Router>
         <Switch>
-          <Route
-            exact
-            path="/"
-            render={props => (user ? <Redirect to="/game" /> : <LoginForm {...props} />)}
-          />
-          <Route path="/game" component={Game} />
+          <PrivateRoute exact path="/" component={Game} />
+          <PrivateRoute exact path="/game" component={Game} />
           <Route render={() => <h2>Page not found</h2>} />
         </Switch>
       </Router>
--- training-react/src/app/components/Game/index.js
+++ training-react/src/app/components/Game/index.js
@@ -18,6 +18,10 @@ class Game extends Component {
     isWinner: null
   };
 
+  componentDidMount() {
+    this.props.history.push('/game');
+  }
+
   getStatus = winner => {
     let status;
     if (winner) {
